67293f11728b455529efce63c47afe7676f1599b
trishan9 <mailtotrishan@gmail.com>
2023-07-12 17:53:41 +0545

    feat: added contents on navbar and footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,19 @@
+import { HeartIcon } from "@heroicons/react/24/solid";
+
 const Footer = () => {
-    return (
-      <footer className="w-full p-4 text-center text-white py-28 bg-brand-black">
-        Footer
-      </footer>
-    )
-  }
-  
-  export default Footer
+  return (
+    <footer className="flex items-center justify-center w-full gap-1 p-4 py-10 font-medium text-center text-white bg-brand-black">
+      <p className="font-semibold">
+        Anime<span className="text-state-blue">Sansar</span>
+      </p>
+
+      <div className="flex items-center justify-center gap-1">
+        <p>- Made with</p>
+        <HeartIcon className="w-5 text-state-red" />
+        <p>by Trishan Wagle</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,9 +1,12 @@
 const NavBar = () => {
   return (
     <nav className="w-full p-6 text-white bg-[#273043]">
-      NavBar
+      <p className="text-2xl font-bold">
+        <span>Anime</span>
+        <span className="text-state-blue">Sansar</span>
+      </p>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
