6613894f4fee20d762b7794eb3d270cdcce4b971
Trishan Wagle <mailtotrishan@gmail.com>
2023-09-24 12:30:28 +0545

    fix: changed footer layout and added social links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,25 +1,59 @@
-import { HeartIcon } from "@heroicons/react/24/solid";
+const socialMedia = [
+  {
+    platform: "linkedin",
+    url: "https://linkedin.com/in/trishan9",
+    imageUrl: "https://img.icons8.com/ios-filled/50/ffffff/linkedin.png",
+  },
+
+  {
+    platform: "github",
+    url: "https://github.com/trishan9",
+    imageUrl: "https://img.icons8.com/ios-filled/50/ffffff/github.png",
+  },
+
+  {
+    platform: "medium",
+    url: "https://medium.com/@trishan9",
+    imageUrl: "https://img.icons8.com/ios-filled/50/ffffff/medium-logo.png",
+  },
+
+  {
+    platform: "twitter",
+    url: "https://twitter.com/trishan999",
+    imageUrl: "https://img.icons8.com/ios-filled/50/ffffff/twitterx.png",
+  },
+];
 
 const Footer = () => {
   return (
-    <footer className="flex items-center justify-center w-full gap-1 p-4 py-10 font-medium text-center text-white bg-brand-black">
-      <p className="font-semibold">
-        Anime<span className="text-state-blue">Sansar</span>
-      </p>
-
-      <div className="flex items-center justify-center gap-1">
-        <p>- Made with</p>
-        <HeartIcon className="w-5 text-state-red" />
-        <p>
-          by
+    <footer className="flex items-start justify-between w-full px-5 py-20 font-medium text-center text-white md:py-10 md:pb-20 lg:px-72 bg-brand-black">
+      <div className="flex flex-col items-start">
+        <p className="text-xl font-semibold">
+          Anime<span className="text-state-blue">Sansar&nbsp;</span>
+        </p>
+        <p className="text-sm">
+          By{" "}
           <a
-            href="https://github.com/trishan9"
             target="_blank"
-            className="ml-2 underline hover:no-underline"
+            href="https://github.com/trishan9"
+            className="underline cursor-pointer hover:no-underline"
           >
             Trishan Wagle
           </a>
         </p>
+      </div>
+
+      <div className="flex items-center justify-center gap-4">
+        {socialMedia.map((data) => (
+          <a target="_blank" href={data.url}>
+            <img
+              width="30"
+              height="30"
+              src={data.imageUrl}
+              alt={data.platform}
+            />
+          </a>
+        ))}
       </div>
     </footer>
   );
